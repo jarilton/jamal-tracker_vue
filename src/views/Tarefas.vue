3ad1128524f397ec1613f28464b0336dee7960c9
Jarilton Junior Silva Pereira <jariltonpereira02@hotmail.com>
2022-05-27 23:07:00 -0300

    modal de edição

--- src/views/Tarefas.vue
+++ src/views/Tarefas.vue
@@ -4,7 +4,31 @@
     <Box v-if="semTarefas">
       Você não está muito produtivo hoje <span class="has-text-weight-bold">:(</span>
     </Box>
-    <Tarefa v-for="(tarefa, index) in tarefas" :tarefa="tarefa" :key="index" />
+    <Tarefa
+      v-for="(tarefa, index) in tarefas"
+      :tarefa="tarefa"
+      :key="index"
+      @aoTarefaClicada="selecionarTarefa"
+    />
+    <div class="modal" :class="{ 'is-active': tarefaSelecionada }" v-if="tarefaSelecionada"> 
+      <div @click="fecharModal" class="modal-background"></div>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">Editando tarefa</p>
+          <button @click="fecharModal" class="delete" aria-label="close"></button>
+        </header>
+        <section class="modal-card-body">
+          <div class="field">
+            <label for="descricaoDaTarefa" class="label"> Descrição da tarefa </label>
+            <input type="text" class="input" v-model="tarefaSelecionada.descricao" id="descricaoDaTarefa" />
+          </div>
+        </section>
+        <footer class="modal-card-foot">
+          <button class="button is-success">Salvar</button>
+          <button @click="fecharModal" class="button">Cancelar</button>
+        </footer>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -24,9 +48,20 @@ export default defineComponent({
     Tarefa,
     Box,
   },
+  data() {
+    return {
+      tarefaSelecionada: null as ITarefa | null,
+    };
+  },
   methods: {
     salvarTarefa(tarefa: ITarefa): void {
       this.store.dispatch(CADASTRAR_TAREFA, tarefa);
+    },
+    selecionarTarefa(tarefa: ITarefa) {
+      this.tarefaSelecionada = tarefa;
+    },
+    fecharModal() {
+      this.tarefaSelecionada = null;
     },
   },
   computed: {
