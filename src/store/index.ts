6f91c23da21377a7125f7a6b202eb5c595ce8b7a
Jarilton Junior Silva Pereira <jariltonpereira02@hotmail.com>
2022-05-27 22:11:53 -0300

    migrando as tarefas para a store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,16 @@
 import IProjeto from "@/interfaces/IProjeto";
 import { createStore, Store, useStore as vuexUseStore } from "vuex";
 import { InjectionKey } from 'vue'
-import { ADICIONA_PROJETO, ALTERA_PROJETO, DEFINIR_PROJETOS, EXCLUIR_PROJETO, NOTIFICAR } from "./tipo-mutacoes";
+import { ADICIONA_PROJETO, ALTERA_PROJETO, DEFINIR_PROJETOS, DEFINIR_TAREFAS, EXCLUIR_PROJETO, NOTIFICAR } from "./tipo-mutacoes";
 import { INotificacao } from "@/interfaces/INotificacao";
-import { ALTERAR_PROJETOS, CADASTRAR_PROJETOS, OBTER_PROJETOS, REMOVER_PROJETOS } from "./tipo-acoes";
+import { ALTERAR_PROJETOS, CADASTRAR_PROJETOS, OBTER_PROJETOS, OBTER_TAREFAS, REMOVER_PROJETOS } from "./tipo-acoes";
 import http from "@/http";
+import ITarefa from "@/interfaces/ITarefa";
 
 interface Estado {
     projetos: IProjeto[],
-    notificacoes: INotificacao[]
+    notificacoes: INotificacao[],
+    tarefas: ITarefa[]
 }
 
 export const key: InjectionKey<Store<Estado>> = Symbol()
@@ -16,7 +18,8 @@ export const key: InjectionKey<Store<Estado>> = Symbol()
 export const store = createStore<Estado>({
     state: {
         projetos: [],
-        notificacoes: []
+        notificacoes: [],
+        tarefas: [],
     },
     mutations: {
         [ADICIONA_PROJETO](state, nomeDoProjeto: string) {
@@ -35,6 +38,9 @@ export const store = createStore<Estado>({
         },
         [DEFINIR_PROJETOS](state, projetos: IProjeto[]) {
             state.projetos = projetos
+        },
+        [DEFINIR_TAREFAS](state, tarefas: ITarefa[]) {
+            state.tarefas = tarefas
         },
         [NOTIFICAR] (state, novaNotificacao: INotificacao) {
 
@@ -62,7 +68,11 @@ export const store = createStore<Estado>({
         [REMOVER_PROJETOS] ({ commit }, id: string) {
             return http.delete(`/projetos/${id}`)
                 .then(() => commit(EXCLUIR_PROJETO, id) )
-        }
+        },
+        [OBTER_TAREFAS] ({ commit }) {
+            http.get('tarefas')
+                .then(response => commit(DEFINIR_TAREFAS, response.data))
+        },
     }
 })
 
